6ce44ea2374868e36005f386dbc941ec3ceae0ae
Miraj Kumar <kumar.miraj@gmail.com>
2024-06-02 20:39:25 -0600

    New changes

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
 console.log("Hello how are you");
+
+let random = Math.random();
+
+if (random <= 0.5) {
+    console.log(`Heheheh ${random}`)
+}
+else if (random <= 0.7) {
+    console.log(`hehehe is more than .5 ${random}`);
+}
+else {
+    console.log("bhak burbak");
+}
+
+let colors = ["red", "yello", "green"];
+console.log(colors);
